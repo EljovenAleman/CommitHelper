c540edce22ab4fcdcfe1a8846ed1b2bd0169fd4a
EljovenAleman <diegobettendorf@gmail.com>
2022-01-06 10:43:03 -0300

    Primer commit

---
+++ CommitHelper/Assets/Editor/CommitHelper.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+using System;
+using Debug = UnityEngine.Debug;
+
+public class CommitHelper : EditorWindow
+{
+    string myString = "Inserte su mensaje para commitear";
+
+    [MenuItem("CustomExtensions/CommitHelper")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<CommitHelper>();
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginHorizontal();
+
+        myString = EditorGUILayout.TextField("Inserte mensaje", myString);
+
+        if(GUILayout.Button("Commit"))
+        {
+            Commit();
+        }
+
+        GUILayout.EndHorizontal();
+    }
+
+    private void Commit()
+    {
+        Debug.Log("Se abre el cmd");
+
+        Process process = Process.Start("cmd.exe");
+    }
+}
